0260a4ec6902158a33bfc5122514412cba282bef
WesterLakerveld <220675@student.glu.nl>
2022-12-06 10:47:05 +0100

    module4 workshop1

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+var eersteVariabele = "test";
+console.log(eersteVariabele);
+
+var voornaam = "Wester"; // wijzig dit in jouw eigen voornaam
+var tussenvoegsel = " "; // wijzig dit in jouw eigen eventuele tussenvoegsel
+var achternaam = "Lakerveld"; // wijzig dit in jouw eigen achternaam
+var volledigeNaam = "Wester Lakerveld"; // plak hier de 3 bovenstaande variabelen aan elkaar, met spaties
+var geboortedag = " 15"; // je geboortedag
+var geboortemaand = "02"; // je geboortemaand
+var geboortejaar = "2005"; // je geboortejaar
+var huidig_jaartal = "2022"// het huidige jaar;
+var leeftijd = 17; // bereken hier jouw leeftijd op basis van de eerder ingevulde geboortegegevens.
+var dag_van_het_jaar = 6; // bereken hier hoeveelste dag van dit kalenderjaar het is
+var straat = "van Bemmelstraat"; // je straat;
+var huisnummer = "44"; // je huisnummer;
+var woonplaats = "Amersfoort"; // je woonplaats;
+var woonachtig_in_utrecht = ""; // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
+var aantal_studenten_in_deze_klas = 21;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = 36.96; // bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+
+/* toon de waardes hieronder op het scherm gescheiden door het html: '<br>' element voor een nieuwe-regel
+  bijvoorbeeld:
+  document.write("Voornaam: " + voornaam);
+  document.write("<br>");
+  document.write("Achternaam: " + achternaam);
+  enzovoorts, zorg dat alle bovenstaande variabelen op het scherm komen
+*/
+
+
+document.write("Voornaam: " + voornaam);
+document.write("<br>");
+document.write("Achternaam: " + achternaam);
+document.write("<br>");
+document.write("Volledige naam: " + volledigeNaam)
+document.write("<br>")
+document.write("geboorte dag: " + geboortedag)
+document.write("<br>")
+document.write("geboorte maand: " + geboortemaand)
+document.write("<br>")
+document.write("geboorte jaar: " + geboortejaar)
+document.write("<br>")
+document.write("huidig jaartal: " + huidig_jaartal)
+document.write("<br>")
+document.write("dag van het jaar: " + dag_van_het_jaar)
+document.write("<br>")
+document.write("straat: " + straat)
+document.write("<br>")
+document.write("huisnummer: " + huisnummer)
+document.write("<br>")
+document.write("woonplaats: " + woonplaats)
+document.write("<br>")
+document.write("aantal studenten: " + aantal_studenten_in_deze_klas  )
+document.write("<br>")
+document.write("kosten frikandel broodje: " + kosten_van_frikandelbroodje)
+document.write("<br>")
+document.write("totale lunch kosten: " + totale_lunchkosten)
+document.write("<br>")
+
+
